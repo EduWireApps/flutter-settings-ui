ef7aa7d9953ad6a0368ed1282c01f701b5c6232d
Androz2091 <androz2091@gmail.com>
2021-04-21 08:39:03 +0200

    fix: adjust padding

--- lib/src/settings_section.dart
+++ lib/src/settings_section.dart
@@ -92,7 +92,7 @@ class SettingsSection extends AbstractSection {
           child: subtitle,
         ),
       ListView.separated(
-        padding: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.only(bottom: 15, top: 5),
         physics: NeverScrollableScrollPhysics(),
         shrinkWrap: true,
         itemCount: tiles!.length,
